319de6dbf75845db0d7d4eb1e6f9bf0b27779d07
jeffrothwell <rothwelljeff@gmail.com>
2018-02-14 15:45:47 -0500

    complete assignment

---
+++ cats.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  var summonCatsButton = document.querySelector('.summon-cats');
+
+  summonCatsButton.addEventListener('click', function(){
+    $.ajax({
+      url: "http://bitkittens.herokuapp.com/cats.json",
+      method: "GET",
+      dataType: "JSON"
+    }).done(function(responseData){
+      // clear all the existing cat-box divs to make room for the new kitties
+      document.querySelectorAll('.cat-box').forEach(function(box){
+        box.innerHTML = "";
+      });
+      // now place each cat image in a div 
+      for (var i = 0; i < responseData.cats.length; i++) {
+        var imgURL = responseData.cats[i].photo;
+        imgEl = document.createElement('img');
+        imgEl.src = imgURL
+        document.querySelector('#cat' + (i + 1)).appendChild(imgEl);
+      }
+    })
+  })
+});
